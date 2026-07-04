00ef8614a83790f7fc37ff49514d782fb786cdda
Anfas Lucknowi <anfaslucknowi9479@gmail.com>
2026-07-04 18:14:09 +0530

    Remove Razorpay code

--- A-Luck cart-full-stack/backend/controllers/orderController.js
+++ A-Luck cart-full-stack/backend/controllers/orderController.js
@@ -171,4 +171,4 @@ const updateStatus = async (req,res) => {
     }
 }
 
-export {verifyRazorpay, verifyStripe ,placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
+export { verifyStripe ,placeOrder, placeOrderStripe, allOrders, userOrders, updateStatus}
